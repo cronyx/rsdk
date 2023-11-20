7229366fab2b37fe61fc3854c081819360060796
cronyx <xcronyx@gmail.com>
2023-11-21 00:44:19 +0300

    initial

---
+++ lib/gcc/mips-linux-uclibc/6.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/RTDOMAIN/jenkins/workspace/sdk-builder/gcc-6/configure --prefix=/tmp/builder-jenkins/rsdk-6.5.0-3222/release/rsdk-6.5.0/linux/uclibc --build=i686-pc-linux --host=i686-pc-linux --target=mips-linux-uclibc --enable-languages=c,c++ --enable-threads --enable-shared --enable-lto --enable-multilib --enable-target-optspace --enable-__cxa_atexit --enable-linker-build-id --disable-sjlj-exceptions --disable-decimal-float --enable-tls --disable-nls --disable-libatomic --disable-libssp --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libffi --disable-libitm --disable-libstdc__-v3 --disable-libsanitizer --disable-fixed-point --with-arch=4181 --with-gnu-ld --with-gnu-as --with-dwarf2 --with-headers=yes --with-sdk=rsdk --with-sysroot='/tmp/builder-jenkins/rsdk-6.5.0-3222/release/rsdk-6.5.0/linux/uclibc' --with-build-sysroot=/tmp/builder-jenkins/rsdk-6.5.0-3222/release/rsdk-6.5.0/linux/uclibc --with-pkgversion='Realtek RSDK-6.5.0 Build 3222' --with-mpfr=/tmp/builder-jenkins/rsdk-6.5.0-3222/stage/rsdk-6.5.0/uclibc/mpfr-build/out --with-gmp=/tmp/builder-jenkins/rsdk-6.5.0-3222/stage/rsdk-6.5.0/uclibc/gmp-build/out --with-mpc=/tmp/builder-jenkins/rsdk-6.5.0-3222/stage/rsdk-6.5.0/uclibc/mpc-build/out --with-mips-plt";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "4181" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
